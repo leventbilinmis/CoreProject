9eb0abf2c000fbd575a11f966b96d335bc9b3e91
Lenovo <Lenovo@DESKTOP-8O3FVNT>
2020-04-18 19:28:26 +0300

    UnitofWork tanımlama

---
+++ Core/UnitOfWorks/IUnitOfWorks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Repositories;
+
+namespace Core.UnitOfWorks
+{
+    public interface IUnitOfWorks
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
