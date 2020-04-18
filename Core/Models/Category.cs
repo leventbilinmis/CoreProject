a33a2522cf2bae2d12ca003a563c6ca2dc95a8a3
Lenovo <Lenovo@DESKTOP-8O3FVNT>
2020-04-18 17:48:32 +0300

    Model Oluşturma İşlemleri

--- Core/Models/Category.cs
+++ Core/Models/Category.cs
@@ -6,9 +6,19 @@ namespace Core.Models
 {
     public class Category
     {
+       
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public bool IsDeleted { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
 
     }
 }
